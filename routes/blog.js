8ad5670347056327e044ab5d9a98f1cc10d44bb4
JapSab <sabaj127001@gmail.com>
2022-12-28 21:08:46 +0400

    finished fixing edit functionality

--- routes/blog.js
+++ routes/blog.js
@@ -33,32 +33,46 @@ router.get('/edit/:id', auth, async (req, res) => {
  })
  
 
+router.put('/:id', async (req, res, next) => {
+    req.article = await Article.findById(req.params.id)
+    next()
+}, saveArticleAndRedirect('edit'));
+
+
+
 router.get('/:slug', async (req, res) => {
     const article = await Article.findOne({slug: req.params.slug})
     if (article == null) res.redirect('/blog/home')
     res.render('articles/show', {article: article})
 })
 
-router.post('/', auth, async (req, res) => {
-    let article = new Article({
-        title: req.body.title,
-        description: req.body.description,
-        markdown: req.body.markdown
-     
-     });
-     try{
-         article = await article.save();
-         res.redirect(`blog/${article.slug}`)
-     } catch (error) {
-        res.render('articles/new', {article: article})
-     }
-})
+router.post('/', auth, async (req, res, next) => {
+    req.article = new Article()
+    next()
+}, saveArticleAndRedirect('new'))
 
 
 router.delete('/:id', async (req, res) => {
     await Article.findByIdAndDelete(req.params.id)
     res.redirect('/blog/home')
 })
+
+
+function saveArticleAndRedirect(path) {
+    return async (req, res) => {
+        let article = req.article
+            article.title = req.body.title
+            article.description = req.body.description
+            article.markdown = req.body.markdown
+         
+         try{
+             article = await article.save();
+             res.redirect('/blog/home')
+         } catch (error) {
+            res.render(`articles/${path}`, {article: article})
+         }
+    }
+};
 
 
 
